07cbdfaf4b1eec56681d7322b81f0088e397c13a
Cosmopolitan-Ou <jimou123456@outlook.com>
2023-03-29 13:50:23 +0800

    run.R update

--- run.R
+++ run.R
@@ -3,14 +3,13 @@ library(shiny)
 library(shinyWidgets)
 
 
-port <- Sys.getenv('PORT')
-
 # source R files
 source(file.path(getwd(), 'utils.R'), encoding="utf-8")
 source(file.path(getwd(), 'ui.R'), encoding="utf-8")
 source(file.path(getwd(), 'server.R'), encoding="utf-8")
 
 # run the application
+# port <- Sys.getenv('PORT')
 # shiny::runApp(
 #   appDir = getwd(),
 #   host = '0.0.0.0',
