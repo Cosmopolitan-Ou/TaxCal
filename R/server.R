04c37e05cc7f47b6acb770b2954df1b838a13747
Cosmopolitan-Ou <jimou123456@outlook.com>
2023-03-30 00:07:18 +0800

    wording update

--- R/server.R
+++ R/server.R
@@ -18,7 +18,7 @@ server <- function(input, output, session) {
         output$value2 <- renderText({paste0("总抵扣额：￥", vals[[2]], " (含免税额￥", BASE_DEDUCT, ")")})
         output$value3 <- renderText({paste0("应纳税所得额（纳税基数）：￥", vals[[3]])})
         output$value4 <- renderText({paste0("应纳税额：￥", vals[[4]], ifelse(vals[[6]] == METHOD[1], paste0(" (含奖金扣税", BONUS_RATE * 100, "%)"), ""))})
-        output$value5 <- renderText({paste0("税后总收入（扣除三险一金）：￥", vals[[5]])})
+        output$value5 <- renderText({paste0("税后总收入(扣除三险一金): ￥", vals[[5]])})
         output$tax_title <- renderText({"扣税明细："})
         output$tax_details1 <- renderText({"注 1：若扣税方式为单独计税，则奖金基数为：年度奖金总收入。累计基数为：年度薪资总收入 - ￥60000 - 年度三险一金总金额 (个人部分) - 专项附加扣除金额"})
         output$tax_details2 <- renderText({"注 2：若扣税方式为合并计税，则奖金基数为：￥0。累计基数为：年度奖金总收入 + 年度薪资总收入 - ￥60000 - 年度三险一金总金额 (个人部分) - 专项附加扣除金额"})
